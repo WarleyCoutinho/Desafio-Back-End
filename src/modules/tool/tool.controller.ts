93ba36969d38a2c524ca83c06feb9416a6189cb8
Warley Coutinho <warleycoutinho@icloud.com>
2022-10-03 13:25:02 -0300

    completing list tools by id with nest js.

--- src/modules/tool/tool.controller.ts
+++ src/modules/tool/tool.controller.ts
@@ -13,14 +13,23 @@ import { ToolService } from './tool.service';
 @Controller('tools')
 export class ToolController {
   constructor(private readonly toolService: ToolService) {}
+  // CREATE TOOL
   @Post()
   async create(@Body() data: ToolDTO) {
     return this.toolService.create(data);
   }
+  // LISTA TODOS
   @Get()
   async findAll() {
     return this.toolService.findAll();
   }
+  //LISTAR POR ID
+  @Get(':id')
+  async filters(@Param('id') id: string, @Body() data: ToolDTO) {
+    return this.toolService.filters(id, data);
+  }
+
+  // ATUALIZAR
   @Put(':id')
   async update(@Param('id') id: string, @Body() data: ToolDTO) {
     return this.toolService.update(id, data);
--- src/modules/tool/tool.service.ts
+++ src/modules/tool/tool.service.ts
@@ -5,25 +5,43 @@ import { ToolDTO } from './dto/tool.create.dto';
 @Injectable()
 export class ToolService {
   constructor(private prisma: PrismaService) {}
+  // CREATE TOOL
   async create(data: ToolDTO) {
-    const toolExixts = await this.prisma.tool.findFirst({
-      where: {
-        id: data.id,
-      },
-    });
-    if (toolExixts) {
-      throw new Error('Tool already exists;');
-    }
+    // const toolExixts = await this.prisma.tool.findFirst({
+    //   where: {
+    //     id: data.id,
+    //   },
+    // });
+    // if (toolExixts) {
+    //   throw new Error('Tool already exists;');
+    // }
     const tool = await this.prisma.tool.create({
       data,
     });
     return tool;
     // response.status(200).json({ getIdTool });
   }
-
+  // LISTA TODOS
   async findAll() {
     return this.prisma.tool.findMany();
   }
+  //LISTAR POR ID
+  async filters(id: string, data: ToolDTO) {
+    const toolExixts = await this.prisma.tool.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!toolExixts) {
+      throw new Error('Tool does not exists!');
+    }
+    return await this.prisma.tool.findUnique({
+      where: {
+        id: data.id,
+      },
+    });
+  }
+  // ATUALIZAR
   async update(id: string, data: ToolDTO) {
     const toolExixts = await this.prisma.tool.findUnique({
       where: {
@@ -40,7 +58,7 @@ export class ToolService {
       },
     });
   }
-
+  // DELETAR
   async delete(id: string) {
     const toolExixts = await this.prisma.tool.findUnique({
       where: {
